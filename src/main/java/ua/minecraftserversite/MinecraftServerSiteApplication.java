24aee4adbe7d08210b4510a40ff8e842e5cb7853
“Vitaly <vitalykarpii@gmail.com>
2022-10-06 22:03:14 +0300

    Added controllers Refactored code Removed header from index.html Created header.html, news.html, personal-office.html Added href to <a> tag at footer.html.

--- src/main/java/ua/minecraftserversite/MinecraftServerSiteApplication.java
+++ src/main/java/ua/minecraftserversite/MinecraftServerSiteApplication.java
@@ -2,8 +2,6 @@ package ua.minecraftserversite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MinecraftServerSiteApplication {
--- src/main/java/ua/minecraftserversite/controller/MainController.java
+++ src/main/java/ua/minecraftserversite/controller/MainController.java
@@ -3,11 +3,20 @@ package ua.minecraftserversite.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class MainController {
     @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/news")
+    public String news(Model model) {
+        return "news";
+    }
+
+    @GetMapping("/personal-office")
+    public String personalOffice(Model model) {
+        return "personal-office";
     }
 }
