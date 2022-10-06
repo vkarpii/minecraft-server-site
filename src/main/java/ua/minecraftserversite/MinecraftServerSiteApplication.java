90c02843da3a84133b5f68ae43718867f3096913
Nazar Brynyarsky <nbrynyarsky@gmail.com>
2022-10-06 19:35:04 +0300

    Delete css file

--- src/main/java/ua/minecraftserversite/MinecraftServerSiteApplication.java
+++ src/main/java/ua/minecraftserversite/MinecraftServerSiteApplication.java
@@ -2,6 +2,8 @@ package ua.minecraftserversite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MinecraftServerSiteApplication {
