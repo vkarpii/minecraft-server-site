b962e2cb3a3e48a5e8da07d641b8ca88c65704c3
“Vitaly <vitalykarpii@gmail.com>
2022-10-09 12:20:18 +0300

    Added : login-form.css, news-style.css. Fixed index.html slider (slide.js) Changed : main.css, error.html, index.html, login.html, news.html

--- src/main/resources/static/js/slide.js
+++ src/main/resources/static/js/slide.js
@@ -4,10 +4,14 @@ const btn = document.getElementById("cases");
 const targetDiv2 = document.getElementById("mainPartCases");
 const btn2 = document.getElementById("privileges");
 btn.onclick = function () {
-    targetDiv.style.visibility = 'hidden ';
-    targetDiv2.style.visibility = 'visible ';
+    targetDiv.style.display = "none";
+    targetDiv2.style.display = "flex";
+    /*targetDiv.style.visibility = 'hidden ';
+    targetDiv2.style.visibility = 'visible ';*/
 };
 btn2.onclick = function () {
-    targetDiv2.style.visibility = 'hidden ';
-    targetDiv.style.visibility = 'visible ';
+    targetDiv2.style.display = "none";
+    targetDiv.style.display = "flex";
+    /*targetDiv2.style.visibility = 'hidden ';
+    targetDiv.style.visibility = 'visible ';*/
 };
