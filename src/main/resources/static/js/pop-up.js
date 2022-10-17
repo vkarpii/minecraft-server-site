46aa9051087bb4f2db6637d67e1c230702ccabff
“Vitaly <vitalykarpii@gmail.com>
2022-10-17 12:58:10 +0300

    Updated pop-up

--- src/main/resources/static/js/pop-up.js
+++ src/main/resources/static/js/pop-up.js
@@ -1,5 +1,5 @@
 const openPopUp = document.getElementById('open-pop-up');
-const closePopup = document.getElementById('pop-up-close' );
+const closePopUp = document.getElementById('pop-up-close' );
 const popUp = document.getElementById('pop-up' );
 
 openPopUp.addEventListener('click', function(e){
@@ -10,3 +10,10 @@ openPopUp.addEventListener('click', function(e){
 closePopUp.addEventListener('click', () => {
     popUp.classList.remove('active');
 })
+
+function onClick(){
+    popUp.classList.add('active');
+}
+function clickClose(){
+    popUp.classList.remove('active');
+}
